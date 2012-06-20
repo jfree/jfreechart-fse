fc5fbf9f31bba3099384198ca48fdaea11b3318e
jfree <dave@jfree.org>
2012-06-20 23:11:50 +0200

    Source reformatting.

--- src/main/java/org/jfree/chart/axis/CategoryAxis.java
+++ src/main/java/org/jfree/chart/axis/CategoryAxis.java
@@ -968,98 +968,99 @@ public class CategoryAxis extends Axis implements Cloneable, Serializable {
             throw new IllegalArgumentException("Null 'state' argument.");
         }
 
-        if (isTickLabelsVisible()) {
-            List ticks = refreshTicks(g2, state, plotArea, edge);
-            state.setTicks(ticks);
+        if (!isTickLabelsVisible()) {
+        	return state;
+        }
+        List ticks = refreshTicks(g2, state, plotArea, edge);
+        state.setTicks(ticks);
 
-            int categoryIndex = 0;
-            Iterator iterator = ticks.iterator();
-            while (iterator.hasNext()) {
+        int categoryIndex = 0;
+        Iterator iterator = ticks.iterator();
+        while (iterator.hasNext()) {
 
-                CategoryTick tick = (CategoryTick) iterator.next();
-                g2.setFont(getTickLabelFont(tick.getCategory()));
-                g2.setPaint(getTickLabelPaint(tick.getCategory()));
+            CategoryTick tick = (CategoryTick) iterator.next();
+            g2.setFont(getTickLabelFont(tick.getCategory()));
+            g2.setPaint(getTickLabelPaint(tick.getCategory()));
 
-                CategoryLabelPosition position
-                        = this.categoryLabelPositions.getLabelPosition(edge);
-                double x0 = 0.0;
-                double x1 = 0.0;
-                double y0 = 0.0;
-                double y1 = 0.0;
-                if (edge == RectangleEdge.TOP) {
-                    x0 = getCategoryStart(categoryIndex, ticks.size(),
-                            dataArea, edge);
-                    x1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
-                            edge);
-                    y1 = state.getCursor() - this.categoryLabelPositionOffset;
-                    y0 = y1 - state.getMax();
-                }
-                else if (edge == RectangleEdge.BOTTOM) {
-                    x0 = getCategoryStart(categoryIndex, ticks.size(),
-                            dataArea, edge);
-                    x1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
-                            edge);
-                    y0 = state.getCursor() + this.categoryLabelPositionOffset;
-                    y1 = y0 + state.getMax();
-                }
-                else if (edge == RectangleEdge.LEFT) {
-                    y0 = getCategoryStart(categoryIndex, ticks.size(),
-                            dataArea, edge);
-                    y1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
-                            edge);
-                    x1 = state.getCursor() - this.categoryLabelPositionOffset;
-                    x0 = x1 - state.getMax();
-                }
-                else if (edge == RectangleEdge.RIGHT) {
-                    y0 = getCategoryStart(categoryIndex, ticks.size(),
-                            dataArea, edge);
-                    y1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
-                            edge);
-                    x0 = state.getCursor() + this.categoryLabelPositionOffset;
-                    x1 = x0 - state.getMax();
-                }
-                Rectangle2D area = new Rectangle2D.Double(x0, y0, (x1 - x0),
-                        (y1 - y0));
-                Point2D anchorPoint = RectangleAnchor.coordinates(area,
-                        position.getCategoryAnchor());
-                TextBlock block = tick.getLabel();
-                block.draw(g2, (float) anchorPoint.getX(),
-                        (float) anchorPoint.getY(), position.getLabelAnchor(),
-                        (float) anchorPoint.getX(), (float) anchorPoint.getY(),
-                        position.getAngle());
-                Shape bounds = block.calculateBounds(g2,
-                        (float) anchorPoint.getX(), (float) anchorPoint.getY(),
-                        position.getLabelAnchor(), (float) anchorPoint.getX(),
-                        (float) anchorPoint.getY(), position.getAngle());
-                if (plotState != null && plotState.getOwner() != null) {
-                    EntityCollection entities
-                            = plotState.getOwner().getEntityCollection();
-                    if (entities != null) {
-                        String tooltip = getCategoryLabelToolTip(
-                                tick.getCategory());
-                        entities.add(new CategoryLabelEntity(tick.getCategory(),
-                                bounds, tooltip, null));
-                    }
-                }
-                categoryIndex++;
+            CategoryLabelPosition position
+                    = this.categoryLabelPositions.getLabelPosition(edge);
+            double x0 = 0.0;
+            double x1 = 0.0;
+            double y0 = 0.0;
+            double y1 = 0.0;
+            if (edge == RectangleEdge.TOP) {
+                x0 = getCategoryStart(categoryIndex, ticks.size(), dataArea, 
+                        edge);
+                x1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea, 
+                        edge);
+                y1 = state.getCursor() - this.categoryLabelPositionOffset;
+                y0 = y1 - state.getMax();
             }
-
-            if (edge.equals(RectangleEdge.TOP)) {
-                double h = state.getMax() + this.categoryLabelPositionOffset;
-                state.cursorUp(h);
-            }
-            else if (edge.equals(RectangleEdge.BOTTOM)) {
-                double h = state.getMax() + this.categoryLabelPositionOffset;
-                state.cursorDown(h);
+            else if (edge == RectangleEdge.BOTTOM) {
+                x0 = getCategoryStart(categoryIndex, ticks.size(), dataArea, 
+                        edge);
+                x1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
+                        edge);
+                y0 = state.getCursor() + this.categoryLabelPositionOffset;
+                y1 = y0 + state.getMax();
             }
             else if (edge == RectangleEdge.LEFT) {
-                double w = state.getMax() + this.categoryLabelPositionOffset;
-                state.cursorLeft(w);
+                y0 = getCategoryStart(categoryIndex, ticks.size(), dataArea, 
+                        edge);
+                y1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
+                        edge);
+                x1 = state.getCursor() - this.categoryLabelPositionOffset;
+                x0 = x1 - state.getMax();
             }
             else if (edge == RectangleEdge.RIGHT) {
-                double w = state.getMax() + this.categoryLabelPositionOffset;
-                state.cursorRight(w);
+                y0 = getCategoryStart(categoryIndex, ticks.size(), dataArea, 
+                        edge);
+                y1 = getCategoryEnd(categoryIndex, ticks.size(), dataArea,
+                        edge);
+                x0 = state.getCursor() + this.categoryLabelPositionOffset;
+                x1 = x0 - state.getMax();
             }
+            Rectangle2D area = new Rectangle2D.Double(x0, y0, (x1 - x0),
+                    (y1 - y0));
+            Point2D anchorPoint = RectangleAnchor.coordinates(area,
+                    position.getCategoryAnchor());
+            TextBlock block = tick.getLabel();
+            block.draw(g2, (float) anchorPoint.getX(),
+                    (float) anchorPoint.getY(), position.getLabelAnchor(),
+                    (float) anchorPoint.getX(), (float) anchorPoint.getY(),
+                    position.getAngle());
+            Shape bounds = block.calculateBounds(g2,
+                    (float) anchorPoint.getX(), (float) anchorPoint.getY(),
+                    position.getLabelAnchor(), (float) anchorPoint.getX(),
+                    (float) anchorPoint.getY(), position.getAngle());
+            if (plotState != null && plotState.getOwner() != null) {
+                EntityCollection entities
+                        = plotState.getOwner().getEntityCollection();
+                if (entities != null) {
+                    String tooltip = getCategoryLabelToolTip(
+                            tick.getCategory());
+                    entities.add(new CategoryLabelEntity(tick.getCategory(),
+                            bounds, tooltip, null));
+                }
+            }
+            categoryIndex++;
+        }
+
+        if (edge.equals(RectangleEdge.TOP)) {
+            double h = state.getMax() + this.categoryLabelPositionOffset;
+            state.cursorUp(h);
+        }
+        else if (edge.equals(RectangleEdge.BOTTOM)) {
+            double h = state.getMax() + this.categoryLabelPositionOffset;
+            state.cursorDown(h);
+        }
+        else if (edge == RectangleEdge.LEFT) {
+            double w = state.getMax() + this.categoryLabelPositionOffset;
+            state.cursorLeft(w);
+        }
+        else if (edge == RectangleEdge.RIGHT) {
+            double w = state.getMax() + this.categoryLabelPositionOffset;
+            state.cursorRight(w);
         }
         return state;
     }
