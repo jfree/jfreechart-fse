1809d85417807d56f7b3ea0ccd028d866fa45272
dgilbert <dgilbert@192.168.1.89>
2012-09-23 12:59:39 +0200

    Fix FindBugs warnings.

--- src/test/java/org/jfree/chart/renderer/junit/LookupPaintScaleTest.java
+++ src/test/java/org/jfree/chart/renderer/junit/LookupPaintScaleTest.java
@@ -120,16 +120,16 @@ public class LookupPaintScaleTest extends TestCase {
         assertTrue(g1.equals(g2));
 
         // check independence
-        g1.add(new Double(0.5), Color.RED);
+        g1.add(0.5, Color.RED);
         assertFalse(g1.equals(g2));
-        g2.add(new Double(0.5), Color.RED);
+        g2.add(0.5, Color.RED);
         assertTrue(g1.equals(g2));
 
         // try with gradient paint
         g1 = new LookupPaintScale(1.0, 2.0, new GradientPaint(1.0f, 2.0f,
                 Color.RED, 3.0f, 4.0f, Color.green));
-        g1.add(new Double(1.5), new GradientPaint(1.0f, 2.0f, Color.RED, 3.0f,
-                4.0f, Color.BLUE));
+        g1.add(1.5, new GradientPaint(1.0f, 2.0f, Color.RED, 3.0f, 4.0f,
+                Color.BLUE));
         g2 = null;
         try {
             g2 = (LookupPaintScale) g1.clone();
