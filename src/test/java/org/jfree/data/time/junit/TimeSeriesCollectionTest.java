4b56718c14b9657fb0380fc3f805988840c24f4f
Michael Clarke <michael.m.clarke@gmail.com>
2012-12-02 11:46:47 +0000

    Pre conversion - fix broken unit tests

--- src/test/java/org/jfree/data/time/junit/TimeSeriesCollectionTest.java
+++ src/test/java/org/jfree/data/time/junit/TimeSeriesCollectionTest.java
@@ -354,21 +354,21 @@ public class TimeSeriesCollectionTest extends TestCase {
 
         s1.add(new Year(2008), 8.0);
         r = DatasetUtilities.findDomainBounds(dataset, visibleSeriesKeys, true);
-        assertEquals(1199142000000.0, r.getLowerBound(), EPSILON);
-        assertEquals(1230764399999.0, r.getUpperBound(), EPSILON);
+        assertEquals(1199145600000.0, r.getLowerBound(), EPSILON);
+        assertEquals(1230767999999.0, r.getUpperBound(), EPSILON);
 
         TimeSeries s2 = new TimeSeries("S2");
         dataset.addSeries(s2);
         s2.add(new Year(2009), 9.0);
         s2.add(new Year(2010), 10.0);
         r = DatasetUtilities.findDomainBounds(dataset, visibleSeriesKeys, true);
-        assertEquals(1199142000000.0, r.getLowerBound(), EPSILON);
-        assertEquals(1230764399999.0, r.getUpperBound(), EPSILON);
+        assertEquals(1199145600000.0, r.getLowerBound(), EPSILON);
+        assertEquals(1230767999999.0, r.getUpperBound(), EPSILON);
 
         visibleSeriesKeys.add("S2");
         r = DatasetUtilities.findDomainBounds(dataset, visibleSeriesKeys, true);
-        assertEquals(1199142000000.0, r.getLowerBound(), EPSILON);
-        assertEquals(1293836399999.0, r.getUpperBound(), EPSILON);
+        assertEquals(1199145600000.0, r.getLowerBound(), EPSILON);
+        assertEquals(1293839999999.0, r.getUpperBound(), EPSILON);
 
         // restore the default time zone
         TimeZone.setDefault(saved);
