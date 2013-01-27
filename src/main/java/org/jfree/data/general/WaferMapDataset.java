3f0b41e35e25f6f2ba441bc88d2788b9b07ad211
David Gilbert <david.gilbert@object-refinery.com>
2013-01-27 20:51:25 +0100

    Fix warnings about new Integer(..)

--- src/main/java/org/jfree/data/general/WaferMapDataset.java
+++ src/main/java/org/jfree/data/general/WaferMapDataset.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.]
  *
  * --------------------
@@ -188,7 +188,7 @@ public class WaferMapDataset extends AbstractDataset {
      * @return The data value.
      */
     public Number getChipValue(int chipx, int chipy) {
-        return getChipValue(new Integer(chipx), new Integer(chipy));
+        return getChipValue(Integer.valueOf(chipx), Integer.valueOf(chipy));
     }
 
     /**
