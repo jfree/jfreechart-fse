b1b07c3ec34ecd69b811c597cfd2e15a1330cf85
David Gilbert <david.gilbert@object-refinery.com>
2013-11-21 10:34:43 +0100

    Merge changes from 1.0.x repo.

--- src/test/java/org/jfree/data/xy/XYSeriesTest.java
+++ src/test/java/org/jfree/data/xy/XYSeriesTest.java
@@ -24,10 +24,10 @@
  * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
  * Other names may be trademarks of their respective owners.]
  *
- * ------------------
- * XYSeriesTests.java
- * ------------------
- * (C) Copyright 2003-2009, by Object Refinery Limited and Contributors.
+ * -----------------
+ * XYSeriesTest.java
+ * -----------------
+ * (C) Copyright 2003-2013, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   -;
@@ -778,4 +778,37 @@ public class XYSeriesTest {
         assertEquals(2.0, s1.getMaxY(), EPSILON);
     }
 
+    @Test
+    public void testSetKey() {
+        XYSeries s1 = new XYSeries("S");
+        s1.setKey("S1");
+        assertEquals("S1", s1.getKey());
+        
+        XYSeriesCollection c = new XYSeriesCollection();
+        c.addSeries(s1);
+        XYSeries s2 = new XYSeries("S2");
+        c.addSeries(s2);
+        
+        // now we should be allowed to change s1's key to anything but "S2"
+        s1.setKey("OK");
+        assertEquals("OK", s1.getKey());
+        
+        try {
+            s1.setKey("S2");
+            fail("Expect an exception here.");
+        } catch (IllegalArgumentException e) {
+            // OK
+        }
+        
+        // after s1 is removed from the collection, we should be able to set
+        // the key to anything we want...
+        c.removeSeries(s1);
+        s1.setKey("S2");
+        
+        // check that removing by index also works
+        s1.setKey("S1");
+        c.addSeries(s1);
+        c.removeSeries(1);
+        s1.setKey("S2");
+    }
 }
