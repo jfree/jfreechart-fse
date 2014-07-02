53279553982964591fe277285d1c237629b5fee1
David Gilbert <david.gilbert@object-refinery.com>
2014-07-02 18:06:01 +0200

    Updated bubble renderer.

---
+++ src/main/java/org/jfree/chart/renderer/xy/ScaleType.java
@@ -0,0 +1,79 @@
+/* ===========================================================
+ * JFreeChart : a free chart library for the Java(tm) platform
+ * ===========================================================
+ *
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:  http://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ----------------------
+ * ScaleType.java
+ * ----------------------
+ * (C) Copyright 2014, by Object Refinery Limited and Contributors.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes:
+ * --------
+ * 25-Apr-2014 : Version 1 (DG);
+ *
+ */
+
+package org.jfree.chart.renderer.xy;
+
+/**
+ * An enumeration of the scaling options for a data item.
+ */
+public enum ScaleType {
+
+    /** Scale using the x-axis. */
+    X_AXIS("X_AXIS"),
+
+    /** Scale using the y-axis. */
+    Y_AXIS("Y_AXIS"),
+
+    /** Scale using both axes. */
+    BOTH_AXES("BOTH_AXES");
+
+    /** The name. */
+    private String name;
+
+    /**
+     * Private constructor.
+     *
+     * @param name  the name.
+     */
+    private ScaleType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns a string representing the object.
+     *
+     * @return The string.
+     */
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+}
