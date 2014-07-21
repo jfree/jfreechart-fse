9ca709dd9face7edba21b8ff9cf730f5fc3c9697
David Gilbert <david.gilbert@object-refinery.com>
2014-07-21 09:43:15 +0200

    Fix Javadoc warnings.

--- src/main/java/org/jfree/chart/renderer/category/StatisticalBarRenderer.java
+++ src/main/java/org/jfree/chart/renderer/category/StatisticalBarRenderer.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2012, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * ---------------------------
  * StatisticalBarRenderer.java
  * ---------------------------
- * (C) Copyright 2002-2012, by Pascal Collet and Contributors.
+ * (C) Copyright 2002-2014, by Pascal Collet and Contributors.
  *
  * Original Author:  Pascal Collet;
  * Contributor(s):   David Gilbert (for Object Refinery Limited);
@@ -102,7 +102,7 @@ import org.jfree.data.statistics.StatisticalCategoryDataset;
  * included in the JFreeChart Demo Collection:
  * <br><br>
  * <img src="../../../../../images/StatisticalBarRendererSample.png"
- * alt="StatisticalBarRendererSample.png" />
+ * alt="StatisticalBarRendererSample.png">
  */
 public class StatisticalBarRenderer extends BarRenderer
         implements CategoryItemRenderer, Cloneable, PublicCloneable,
