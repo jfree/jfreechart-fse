76666eba376d9b99ed40f7fbe097857de3cf75ce
David Gilbert <david.gilbert@object-refinery.com>
2014-07-21 09:42:44 +0200

    Fix Javadoc warnings.

--- src/main/java/org/jfree/chart/renderer/category/StackedBarRenderer.java
+++ src/main/java/org/jfree/chart/renderer/category/StackedBarRenderer.java
@@ -2,7 +2,7 @@
  * JFreeChart : a free chart library for the Java(tm) platform
  * ===========================================================
  *
- * (C) Copyright 2000-2012, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
  *
  * Project Info:  http://www.jfree.org/jfreechart/index.html
  *
@@ -27,7 +27,7 @@
  * -----------------------
  * StackedBarRenderer.java
  * -----------------------
- * (C) Copyright 2000-2012, by Object Refinery Limited and Contributors.
+ * (C) Copyright 2000-2014, by Object Refinery Limited and Contributors.
  *
  * Original Author:  David Gilbert (for Object Refinery Limited);
  * Contributor(s):   Richard Atkinson;
@@ -123,7 +123,7 @@ import org.jfree.data.general.DatasetUtilities;
  * JFreeChart Demo Collection:
  * <br><br>
  * <img src="../../../../../images/StackedBarRendererSample.png"
- * alt="StackedBarRendererSample.png" />
+ * alt="StackedBarRendererSample.png">
  */
 public class StackedBarRenderer extends BarRenderer
         implements Cloneable, PublicCloneable, Serializable {
